fa2af2bae539da5cc01b408aee62d958b8e91281
Nicole Plaza <nico.plaza@duocuc.cl>
2026-04-10 23:16:40 -0400

    Paso 2 y 3 Lógica de negocio

--- src/main/java/com/duoc/LearningPlatform/controller/CursoController.java
+++ src/main/java/com/duoc/LearningPlatform/controller/CursoController.java
@@ -1,5 +1,22 @@
 package com.duoc.LearningPlatform.controller;
 
+import com.duoc.LearningPlatform.model.Curso;
+import com.duoc.LearningPlatform.service.CursoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cursos")
 public class CursoController {
-    
+    @Autowired
+    private CursoService cursoService;
+
+    @GetMapping
+    public List<Curso> listarCursosDisponibles() {
+        return cursoService.obtenerCursosDisponibles();
+    }
 }
--- src/main/java/com/duoc/LearningPlatform/service/CursoService.java
+++ src/main/java/com/duoc/LearningPlatform/service/CursoService.java
@@ -1,5 +1,35 @@
 package com.duoc.LearningPlatform.service;
 
+import com.duoc.LearningPlatform.model.Curso;
+import com.duoc.LearningPlatform.repository.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+@Service
 public class CursoService {
-    
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    private List<Curso> cursosTemporales = new ArrayList<>();
+
+    public CursoService() {
+        cursosTemporales.add(new Curso("Java Básico", "Curso de introducción a Java", true));
+        cursosTemporales.add(new Curso("Python para Data Science", "Curso de Python enfocado en Data Science", false));
+        cursosTemporales.add(new Curso("Spring Boot", "Curso avanzado de Spring Boot", true));
+        cursosTemporales.add(new Curso("Bases de Datos", "Curso de introducción a las Bases de Datos", true));
+        cursosTemporales.add(new Curso("Seguridad en Aplicaciones", "Curso sobre seguridad en aplicaciones", true));
+        cursosTemporales.add(new Curso("Machine Learning", "Curso avanzado de Machine Learning", true));
+    }
+
+    public List<Curso> obtenerCursosDisponibles(){
+        return cursosTemporales.stream()
+                .filter(Curso::isActivo)
+                .sorted(Comparator.comparing(Curso::getNombre))
+                .collect(Collectors.toList());
+    }
 }

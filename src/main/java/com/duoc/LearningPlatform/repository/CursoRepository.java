8f52f02148f59c82bc4ca9a0c71606c52bc8b170
JenniferGonzalezT <je.gonzalezt@duocuc.cl>
2026-04-13 16:57:03 -0400

    Cambios repositorio

--- src/main/java/com/duoc/LearningPlatform/repository/CursoRepository.java
+++ src/main/java/com/duoc/LearningPlatform/repository/CursoRepository.java
@@ -1,50 +1,15 @@
 package com.duoc.LearningPlatform.repository;
 
 import com.duoc.LearningPlatform.model.Curso;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
-public class CursoRepository {
+public interface CursoRepository extends JpaRepository<Curso, Long>{
 
-    // Estructura de almacenamiento temporal
-    private List<Curso> cursos;
+    List<Curso> findByActivoTrue();
 
-    public CursoRepository() {
-    	cursos = new ArrayList<>();
-        
-        // Simulamos la base de datos agregando algunos cursos
-		Curso c1 = new Curso("Java Básico", "Curso de introducción a Java", true);
-        c1.setId(1L);
-        cursos.add(c1);
-
-        Curso c2 = new Curso("Python para Data Science", "Curso de Python enfocado en Data Science", false);
-        c2.setId(2L);
-        cursos.add(c2);
-
-        Curso c3 = new Curso("Spring Boot", "Curso avanzado de Spring Boot", true);
-        c3.setId(3L);
-        cursos.add(c3);
-
-        Curso c4 = new Curso("Bases de Datos", "Curso de introducción a las Bases de Datos", true);
-        c4.setId(4L);
-        cursos.add(c4);
-
-        Curso c5 = new Curso("Seguridad en Aplicaciones", "Curso sobre seguridad en aplicaciones", true);
-        c5.setId(5L);
-        cursos.add(c5);
-
-        Curso c6 = new Curso("Machine Learning", "Curso avanzado de Machine Learning", true);
-        c6.setId(6L);
-        cursos.add(c6);
-    }
-
-    public List<Curso> findByActivoTrue() {
-        return cursos.stream()
-                .filter(Curso::isActivo)
-                .collect(Collectors.toList());
-    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO curso (nombre, descripcion, activo) VALUES ('Java Basico', 'Curso de introduccion a Java', true);
+INSERT INTO curso (nombre, descripcion, activo) VALUES ('Python para Data Science', 'Curso de Python enfocado en Data Science', false);
+INSERT INTO curso (nombre, descripcion, activo) VALUES ('Spring Boot', 'Curso avanzado de Spring Boot', true);
+INSERT INTO curso (nombre, descripcion, activo) VALUES ('Bases de Datos', 'Curso de introduccion a las Bases de Datos', true);
+INSERT INTO curso (nombre, descripcion, activo) VALUES ('Seguridad en Aplicaciones', 'Curso sobre seguridad en aplicaciones', true);
+INSERT INTO curso (nombre, descripcion, activo) VALUES ('Machine Learning', 'Curso avanzado de Machine Learning', true);
